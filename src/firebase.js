a13a7e85ada66d6583b2d859bc438cb28bfd7e15
Volodymyr Dziubanskyi <volodymyr.dziubanskyi@temy.co>
2024-03-07 13:37:17 +0200

    ee-4: init project

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const projectId = import.meta.env.VITE_PROJECT_ID;
+const authDomain = import.meta.env.VITE_AUTH_DOMAIN;
+const storageBucket = import.meta.env.VITE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.VITE_MESSAGING_SENDER_ID;
+const appId = import.meta.env.VITE_APP_ID
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const googleAuthProvider = new GoogleAuthProvider();
---
+++ src/pages/LoginForm.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import { signInWithPopup, createUserWithEmailAndPassword, signInAnonymously, signInWithCredential } from "firebase/auth";
+import { auth, googleAuthProvider } from '../firebase';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonList, IonGrid, IonRow, IonCol } from '@ionic/react';
+import './Tab1.css';
+
+export const LoginForm: React.FC = () => {
+    // const [user, setUser] = useState(auth.currentUser);
+
+    // useEffect(() => {
+    //     const unsub = auth.onAuthStateChanged((storeUser) => {
+    //         if (storeUser != null) {
+    //             return setUser(storeUser);
+    //         }
+    //         signInWithPopup(auth, googleAuthProvider)
+    //             .then(credentials => {
+    //                 setUser(credentials.user)
+    //             }
+    //             )
+    //             .catch(e => console.error(e));
+    //     });
+
+    //     return unsub;
+    // }, [auth]);
+
+    return <IonPage>
+        <IonContent fullscreen>
+            <IonGrid>
+                <IonRow>
+                    <IonCol sizeMd='4' offsetMd="4" sizeSm='12'>
+                        <IonList>
+                            <IonItem>
+                                <IonInput label="Email" type="email" placeholder="email@domain.com"></IonInput>
+                            </IonItem>
+                            <IonItem>
+                                <IonInput label="Password" type="password"></IonInput>
+                            </IonItem>
+                        </IonList>
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
+        </IonContent>
+    </IonPage>
+        ;
+
+}
